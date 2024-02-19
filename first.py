23121766540728bcb02ce4c63e848bb4916ba973
Tshewang Nima <02230114.cst@rub.edu.bt>
2024-02-19 14:38:48 +0600

    Grade

--- first.py
+++ first.py
@@ -1,2 +1,11 @@
-print ("Hello World")
-print ("I am suffering")
+grade = 70
+if grade >= 90:
+    print ("Grade: A")
+elif grade >= 80:
+    print ("Grade: B")
+elif grade >= 70:
+    print ("Grade: C")
+elif grade >= 60:
+    print("Grade: D")
+else:
+    print ("Grade: F")
