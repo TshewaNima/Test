1e12713d14c747f22098352b9bcf8b466347518e
Tshewang Nima <02230114.cst@rub.edu.bt>
2024-02-26 20:38:06 +0600

    classwork

--- classwork.py
+++ classwork.py
@@ -1,7 +1,12 @@
-print ("eligibility for Discount ")
-age = int (input ("Enter your age: "))
-student = input ("Are you a student? (yes/no): ")
-if (age <= 12 or (age >= 13 and age <=  18 and student == "yes" )):
-    print ("YOu are eligible for a discount on the movie ticket.")
+print ("Eligibility for Discount")
+age = int(input("Enter your age: "))
+std = input("Are you a student (yes/no): ")
+
+if age <= 12:
+    print("You are eligible for a discount on the movie ticket")
+    
+elif  13 <= age <= 18 and std == "yes":
+    print("You are eligible for a discount on the movie ticket")
+    
 else:
-    print ("You are not eligible for a discount on the movie ticket")
+    print("You are not eligible for a discount on the movie ticket")
